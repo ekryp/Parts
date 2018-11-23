2fca6d543f823f527315062ef87221baa99c9240
ashishbainade-ekryp <ashish.bainade@ekryp.com>
2018-11-23 16:11:50 +0530

    Basic POST API PostSparePartAnalysis Storing file User Wise

--- backend/app/resources/infinera.py
+++ backend/app/resources/infinera.py
@@ -21,7 +21,8 @@ from flask import _app_ctx_stack, session, request
 import json
 import time
 import urllib
-
+from app import app
+from app import csvs,excel
 
 class GetSparePartAnalysis(Resource):
 
@@ -43,3 +44,51 @@ class GetSparePartAnalysis(Resource):
         }
         return response
 
+class PostSparePartAnalysis(Resource):
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('analysis_name', required=True, location='form')
+        #self.reqparse.add_argument('analysis_time', required=True, location='form')
+        self.reqparse.add_argument('analysis_type', required=True, location='form')
+        self.reqparse.add_argument('replenish_time', required=True, location='form')
+        #self.reqparse.add_argument('file_names', required=True, location='form')
+        self.reqparse.add_argument('user_email_id', required=True, location='form')
+        super(PostSparePartAnalysis, self).__init__()
+
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        dest_folder = request.form.get('user_email_id')
+        analysis_date = str(datetime.now())
+        for file in request.files.getlist('customer_dna_file'):
+
+            name, extension = os.path.splitext(file.filename)
+
+            if extension.lower() == '.csv':
+                dir_path = os.path.join(app.config.get("UPLOADED_CSV_DEST"), dest_folder)
+                full_path = os.path.abspath(dir_path)
+                file.filename = "customer_dna_file_{0}{1}".format(analysis_date, extension.lower())
+                csvs.save(file, folder=dest_folder)
+
+            elif extension.lower() == '.xls' or extension == '.xlsx':
+                dir_path = os.path.join(app.config.get("UPLOADED_EXCEL_DEST"), dest_folder)
+                full_path = os.path.abspath(dir_path)
+                file.filename = "customer_dna_file_{0}{1}".format(analysis_date, extension.lower())
+                excel.save(file, folder=dest_folder)
+
+        for file in request.files.getlist('sap_export_file'):
+
+            name, extension = os.path.splitext(file.filename)
+
+            if extension.lower() == '.csv':
+                dir_path = os.path.join(app.config.get("UPLOADED_CSV_DEST"), dest_folder)
+                full_path = os.path.abspath(dir_path)
+                file.filename = "sap_export_file{0}{1}".format(analysis_date, extension.lower())
+                csvs.save(file, folder=dest_folder)
+
+            elif extension.lower() == '.xls' or extension.lower() == '.xlsx':
+                dir_path = os.path.join(app.config.get("UPLOADED_EXCEL_DEST"), dest_folder)
+                full_path = os.path.abspath(dir_path)
+                file.filename = "sap_export_file{0}{1}".format(analysis_date, extension.lower())
+                excel.save(file, folder=dest_folder)
