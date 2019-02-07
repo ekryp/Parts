b22a8b8245d7408d788615c494a04bcb33466315
nageshwara <nageshwara.vijay@ekryp.com>
2019-02-07 18:31:52 +0530

    API and APP URL change

--- frontend/src/components/constant/constant.js
+++ frontend/src/components/constant/constant.js
@@ -1,2 +1,4 @@
-export const APIURL = 'http://localhost:5000/'
-export const APPURL = 'http://localhost:2323/'
+// export const APIURL = 'http://localhost:5000/'
+// export const APPURL = 'http://localhost:2323/'
+export const APIURL = 'https://staging-ib-services.ekryp.com/'
+export const APPURL = 'https://staging-ib.ekryp.com/'
