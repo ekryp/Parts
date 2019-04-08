f471c112abd2369a6fc06f20134df48bfcb16a64
ashishbainade-ekryp <ashish.bainade@ekryp.com>
2019-04-08 18:18:33 +0530

    Pause upload job if request analysis is running

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python -u  manage.py startserver 2>&1 >/python.log & tail -f /python.log & celery -A app.tasks:celery worker --concurrency=1
+python -u  manage.py startserver 2>&1 >/python.log & tail -f /python.log & celery -A app.tasks:celery worker
