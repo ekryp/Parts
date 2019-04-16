e38b254f37356cbdf26fbbfc0fa351b114ee9295
Khalisaran <khalicharan@redblacktree.com>
2019-04-16 15:41:37 +0530

    changes in the response object

--- ELK/app/devtrack/devtrack.py
+++ ELK/app/devtrack/devtrack.py
@@ -22,11 +22,21 @@ class DevTrackData(Resource):
             r = requests.get(url = URL, data = PARAMS, headers=headers) 
             data = r.json()
             esResponses = data['hits']['hits']
-            responses = []
+            response= {
+                    "devTrack": [],
+                    "releaseNotes": [],
+                    "salesForces": []
+                }
             for doc in esResponses:
                 data = doc["_source"]
-                responses.append(data)
-            return jsonify(data=responses, http_status_code=200)
+                if(doc["_type"] == "devtrack"):
+                    response["devTrack"].append(data)
+                if(doc["_type"] == "releasenotes"):
+                    response["releaseNotes"].append(data)
+                if(doc["_type"] == "salesforce"):
+                    response["salesForce"].append(data)
+            
+            return jsonify(data=response, http_status_code=200)
         except Exception as e:
             print(e)
             return jsonify(msg="Error in Updating,Please try again", http_status_code=400)
