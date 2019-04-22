e7a3af77bb4f4eb7d4549860914cd2ff70cb3bb8
ashishbainade-ekryp <ashish.bainade@ekryp.com>
2019-04-22 12:55:22 +0530

    Integrating pranav's code

--- ELK/app/__init__.py
+++ ELK/app/__init__.py
@@ -29,9 +29,10 @@ app.config['SECRET_KEY'] = 'secret'
 compress.init_app(app)
 
 from app.devtrack.devtrack import DevTrackData
-
+from app.Keywords.keywords import GetMLKeyWords
 
 api.add_resource(DevTrackData, '/getDevTrackData', endpoint='getDevTrackData')
+api.add_resource(GetMLKeyWords, '/get_ml_keywords', endpoint='get_ml_keywords')
 
 app.register_blueprint(api_blueprint)
 api.init_app(app)
