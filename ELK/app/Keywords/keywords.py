44edf5cc255ef30a389065a988e97b3b3fd55723
ashishbainade-ekryp <ashish.bainade@ekryp.com>
2019-04-24 12:59:28 +0530

    COnfiguratble files for ML keyword

--- ELK/app/Keywords/keywords.py
+++ ELK/app/Keywords/keywords.py
@@ -3,6 +3,7 @@ from flask import request
 import requests
 from flask_restful import Resource
 from flask_restful import reqparse
+import pandas as pd
 
 
 class GetMLKeyWords(Resource):
@@ -18,54 +19,35 @@ class GetMLKeyWords(Resource):
             input_ = search_param.lower()
             print("Received Input is {0}".format(input_))
 
+            # Read keywords
+            df = pd.read_excel('Keyword_UI.xlsx')
+            df_e = pd.read_excel('UI_Elastic.xlsx')
+
+            df[df.columns[1]] = df[df.columns[1]].astype(str)
+            df[df.columns[0]] = ' ' + df[df.columns[0]].astype(str) + ' '
+            df[df.columns[0]] = df[df.columns[0]].str.lower()
+            list_keywords = list(df[df.columns[0]])
+            list_tags = list(df[df.columns[1]])
+
+            list_UI_tags = list(df_e[df_e.columns[0]])
+            list_elastic = list(df_e[df_e.columns[1]])
+
             def check(word):
                 if word in input_:
                     return True
                 return False
 
-            keywords = ["DOA", "Dead On Arrival", "Safeboot", "USB-11", "IKE", "Equipment Degrade Alarm", "Timeout",
-                        "Interface Flap", "Upgrade Failed",
-                        "CPUR", "Control Plane UnReachable", "MACSEC", "LOSYNC", "OLOS", "LOS", "Cold Reboot",
-                        "Cold Reset", "Warm Reboot",
-                        "Warm Reset", "Post-FEC BER", "Loss of Connectivity", "Low Voltage", "PIC Init State",
-                        "Fatal Error", "FEC Error",
-                        "Broken", "Critical Alarm", "TOM issue", "False Rx Reading", "False Tx reading", "False Alarm",
-                        "BPOSTFAIL", "IGCC Failure", "DNA Lost Communication"]
-            keywords = [i.lower() for i in keywords]
-
-            desc_split = input_.split()
             ans = ""
-            category =[]
-            if 'rma' in desc_split or 'rmas' in desc_split:
-                ans = ans + 'RMA' + ','
-                category.append('RMA')
-            if 'service return' in input_:
-                ans = ans + 'Service Return' + ','
-                category.append('Service Return')
-            if 'refurbish' in desc_split or 'move the unit to ufg' in input_:
-                ans = ans + 'ECO/Refurbish' + ','
-                category.append('ECO/Refurbish')
-            if check('replace and upgrade') or check('defective'):
-                ans = ans + 'Replace/Upgrade' + ','
-                category.append('Replace/Upgrade')
-            if check('equipment failure') or check('equipment fail') or check('equipment failur') or (
-                check('faulty') and check('equipment')):
-                ans = ans + 'Faulty Equipment' + ','
-                category.append('Faulty Equipment')
-            if check('scrap'):
-                ans = ans + 'Scrap' + ','
-                category.append('Scrap')
-            if check('traffic hit') or check('dropping traffic') or check('network outage') or check('outage') or check(
-                    'traffic glitch') or check('disruption in service'):
-                ans = ans + 'Network Down' + ','
-                category.append('Network Down')
-            if check('continuous reboot') or check('reboot loop'):
-                ans = ans + 'Reboot Loop' + ','
-                category.append('Reboot Loop')
-            for x in keywords:
-                if check(x):
-                    ans = ans + x + ','
-                    category.append(x)
+            set_ = set()
+            for ind, val in enumerate(list_keywords):
+                if check(val) and list_tags[ind] not in set_:
+                    set_.add(list_tags[ind])
+                    ans = ans + list_tags[ind] + ','
 
-            #return ans
-            return jsonify(ml_keywords=category, http_status_code=200)
+            ans_split = ans.split(',')
+            ans_e = []
+            for ind, val in enumerate(list_UI_tags):
+                if val in ans_split:
+                    ans_e.append(list_elastic[ind])
+
+            return jsonify(ml_keywords=ans_e, ml_synonym=ans_split[:-1], http_status_code=200)
