1f9cb18c9f0ab941716bb5ec151fa81c522161fc
ashishbainade-ekryp <ashish.bainade@ekryp.com>
2019-05-20 16:06:03 +0530

    keyword issue fix,rewamp pranav's code

--- ELK/app/Keywords/keywords.py
+++ ELK/app/Keywords/keywords.py
@@ -19,36 +19,15 @@ class GetMLKeyWords(Resource):
             input_ = search_param.lower()
             print("Received Input is {0}".format(input_))
 
-
             # Read keywords
-            df = pd.read_excel('Keyword_UI.xlsx')
-            df_e = pd.read_excel('UI_Elastic.xlsx')
+            df1 = pd.read_excel('Keyword_UI.xlsx')
+            #df2 = pd.read_excel('UI_Elastic.xlsx')
 
-            df[df.columns[1]] = df[df.columns[1]].astype(str)
-            df[df.columns[0]] = df[df.columns[0]].astype(str)
-            df[df.columns[0]] = df[df.columns[0]].str.lower()
-            list_keywords = list(df[df.columns[0]])
-            list_tags = list(df[df.columns[1]])
+            df3 = df1[df1.apply(lambda row: row.astype(str).str.contains(input_, case=False).any(), axis=1)]
+            #df4 = df2[df2.apply(lambda row: row.astype(str).str.contains(input_, case=False).any(), axis=1)]
+            ml_synonym = []
+            for word in df3['Elastic Search Map'].tolist():
+                word = word.split(",")
+                ml_synonym.extend(word)
 
-            list_UI_tags = list(df_e[df_e.columns[0]])
-            list_elastic = list(df_e[df_e.columns[1]])
-
-            def check(word):
-                if word in input_:
-                    return True
-                return False
-
-            ans = ""
-            set_ = set()
-            for ind, val in enumerate(list_keywords):
-                if check(val) and list_tags[ind] not in set_:
-                    set_.add(list_tags[ind])
-                    ans = ans + list_tags[ind] + ','
-
-            ans_split = ans.split(',')
-            ans_e = []
-            for ind, val in enumerate(list_UI_tags):
-                if val in ans_split:
-                    ans_e.append(list_elastic[ind])
-
-            return jsonify(ml_keywords=ans_e, ml_synonym=ans_split[:-1], http_status_code=200)
+            return jsonify(ml_keywords=df3['UI Problem Area Map'].tolist(),  ml_synonym=ml_synonym, http_status_code=200)
