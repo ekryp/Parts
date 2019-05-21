b17dfc6fea7d7fda87ead2bc7b26139c8b3f4c41
ashishbainade-ekryp <ashish.bainade@ekryp.com>
2019-05-21 15:02:20 +0530

    ML keyword issue fix

--- ELK/app/Keywords/keywords.py
+++ ELK/app/Keywords/keywords.py
@@ -18,28 +18,46 @@ class GetMLKeyWords(Resource):
             # take input
             input_ = search_param.lower()
             print("Received Input is {0}".format(input_))
-            ml_keywords = []
-            ml_synonym = []
 
             # Read keywords
-            df1 = pd.read_excel('Keyword_UI.xlsx')
-            # df2 = pd.read_excel('UI_Elastic.xlsx')
+            keyword_df = pd.read_excel('Key Words.xlsx', header=None)
+            keywords = keyword_df[0].tolist()
 
-            for word in input_.split():
+            # get keywords from input
+            found_keyword = []
+            for keyword in keywords:
+                if keyword.lower() in input_:
+                    if keyword not in found_keyword:
+                        found_keyword.append(keyword)
 
-                df3 = df1[df1.apply(lambda row: row.astype(str).str.contains(word, case=False).any(), axis=1)]
-                # df4 = df2[df2.apply(lambda row: row.astype(str).str.contains(word, case=False).any(), axis=1)]
+            print("Keywords found in input are {0}".format(found_keyword))
 
-                for matches in df3['Elastic Search Map'].tolist():
-                    matches = matches.split(",")
-                    for match in matches:
-                        if match not in ml_synonym:
-                            ml_synonym.append(match)
+            # get problem area from keywords
+            problem_area_df = pd.read_excel('UI_Elastic.xlsx')
+            found_problem_area = []
 
-                for matches in df3['UI Problem Area Map'].tolist():
-                    matches = matches.split(",")
-                    for match in matches:
-                        if match not in ml_keywords:
-                            ml_keywords.append(match)
+            for keyword in found_keyword:
+                matched_problem_area = problem_area_df[problem_area_df['Key Words'].str.match(keyword, case=False)]
 
-            return jsonify(ml_keywords=ml_keywords,  ml_synonym=ml_synonym, http_status_code=200)
+                if not matched_problem_area.empty:
+                    for each_problem_area in matched_problem_area['UI Problem Area Map'].tolist():
+                        if each_problem_area not in found_problem_area:
+                            found_problem_area.append(each_problem_area)
+
+            print("Problem Area found are {0}".format(found_problem_area))
+
+            # Get elastic search words
+            elastic_search_df = pd.read_excel('Keyword_UI.xlsx')
+            found_elastic_search_words = []
+
+            for problem_area in found_problem_area:
+                matched_elastic_search_words = elastic_search_df[elastic_search_df['UI Problem Area Map'].str.match(problem_area, case=False)]
+
+                if not matched_elastic_search_words.empty:
+                    for each_elastic_word in matched_elastic_search_words['Elastic Search Map'].tolist():
+                        if each_elastic_word not in found_elastic_search_words:
+                            found_elastic_search_words.append(each_elastic_word)
+
+            print("elastic search words found are {0}".format(found_elastic_search_words))
+
+            return jsonify(ml_keywords=found_problem_area,  ml_synonym=found_elastic_search_words, http_status_code=200)
