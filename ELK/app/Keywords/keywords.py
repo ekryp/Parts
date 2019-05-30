387f06bf7aa37ae580082fefae4513327dfca002
ashishbainade-ekryp <ashish.bainade@ekryp.com>
2019-05-30 12:52:47 +0530

    Finding stopwords, finding double quoted one & mapping with problem area & synonym

--- ELK/app/Keywords/keywords.py
+++ ELK/app/Keywords/keywords.py
@@ -4,6 +4,9 @@ import requests
 from flask_restful import Resource
 from flask_restful import reqparse
 import pandas as pd
+import nltk
+nltk.download('stopwords')
+import re
 
 
 class GetMLKeyWords(Resource):
@@ -18,6 +21,34 @@ class GetMLKeyWords(Resource):
             # take input
             input_ = search_param.lower()
             print("Received Input is {0}".format(input_))
+
+            def find_double_quoted_string(text):
+                # text Regex should return "String 1" or "String 2" or "String3"
+                import re
+                matches = re.findall(r'\"(.+?)\"', text)
+                # matches is now ['String 1', 'String 2', 'String3']
+                return matches
+                # return ",".join(matches)
+                return ', '.join(['"{}"'.format(c) for c in matches])
+
+            one_strings = find_double_quoted_string(input_)
+
+            # Remove one_strings from input_
+
+            for word in one_strings:
+                input_ = input_.replace(word, '')
+                input_ = input_.replace('"', '')
+
+            print("Received Input is {0}".format(input_))
+
+            # Remove Stop words
+            from nltk.corpus import stopwords
+            stop = set(stopwords.words('english'))
+            input = [i for i in input_.lower().split() if i not in stop]
+            removed_stopwords = [i for i in input_.lower().split() if i in stop]
+            input = " ".join(input)
+            input = re.sub('\s+', ' ', input).strip()
+            print(input)
 
             # Read keywords
             keyword_df = pd.read_excel('Key Words.xlsx', header=None)
@@ -47,6 +78,10 @@ class GetMLKeyWords(Resource):
                 if not matched_problem_area.empty:
                     for each_problem_area in matched_problem_area['UI Problem Area Map'].tolist():
                         if each_problem_area not in found_problem_area:
+                            # remove start & end space but ont in between space in words
+                            # ' one two  ' becomes 'one two'
+                            each_problem_area = each_problem_area.lstrip()
+                            each_problem_area = each_problem_area.rstrip()
                             found_problem_area.append(each_problem_area)
 
             print("Problem Area found are {0}".format(found_problem_area))
@@ -54,6 +89,7 @@ class GetMLKeyWords(Resource):
             # Get elastic search words
             elastic_search_df = pd.read_excel('Keyword_UI.xlsx')
             found_elastic_search_words = []
+            mapping = {}
 
             for problem_area in found_problem_area:
                 matched_elastic_search_words = elastic_search_df[elastic_search_df['UI Problem Area Map'].str.match(problem_area, case=False)]
@@ -63,7 +99,15 @@ class GetMLKeyWords(Resource):
                         if each_elastic_word not in found_elastic_search_words:
                             # split multiple words into seperate list item
                             for each_word in each_elastic_word.split(','):
+                                # remove start & end space but ont in between space in words
+                                # ' one two  ' becomes 'one two'
+                                each_word = each_word.lstrip()
+                                each_word = each_word.rstrip()
                                 found_elastic_search_words.append(each_word)
 
+                mapping[problem_area] = found_elastic_search_words
+
             print("elastic search words found are {0}".format(found_elastic_search_words))
-            return jsonify(ml_keywords=found_problem_area,  ml_synonym=found_elastic_search_words, http_status_code=200)
+
+            return jsonify(mapping=mapping, input=input,
+                           one_strings=one_strings, removed_stopwords=removed_stopwords, http_status_code=200)
