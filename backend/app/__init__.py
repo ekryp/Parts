55753eae0b706f9c87b52d42b5f65cf4fdc41021
ashishbainade-ekryp <ashish.bainade@ekryp.com>
2019-06-20 11:56:25 +0530

    Get TopExtended  by IB

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -125,7 +125,7 @@ from app.resources.infinera import GetSparePartAnalysis,PostSparePartAnalysis,Re
     GetMainDashboardCount, GetPieChart, GetTopPons, GetTopDepots, GetTopCustomer, GetTopExtended,\
     GetGrossforSpecificRequest, GetCurrentInventory, GetCurrentNet, GetCurrentIB, GetLatLon, GetAnalysisName, \
     GetErrorRecords, GetSummaryByPONforSpecificRequest, FilterMainDashboard, GetAnalysisDashboardCount, AdvaceSettings,\
-    DNAPreprocess, GetTopPonsIB, GetTopDepotsIB, GetTopCustomerIB
+    DNAPreprocess, GetTopPonsIB, GetTopDepotsIB, GetTopCustomerIB, GetTopExtendedIB
 
 from app.resources.reference import UploadParts, UploadDepot, UploadNode, UploadHighSpare,\
     UploadMisnomer, UploadRatio, UploadEndCustomer, UploadLabDetails
@@ -214,6 +214,7 @@ api.add_resource(DNAPreprocess, '/get_service_states', endpoint='get_service_sta
 api.add_resource(GetTopPonsIB, '/get_top_pons_IB', endpoint='get_top_pons_IB')
 api.add_resource(GetTopDepotsIB, '/get_top_depots_IB', endpoint='get_top_depots_IB')
 api.add_resource(GetTopCustomerIB, '/get_top_customers_IB', endpoint='get_top_customers_IB')
+api.add_resource(GetTopExtendedIB, '/get_top_extended_IB', endpoint='get_top_extended_IB')
 app.register_blueprint(api_blueprint)
 api.init_app(app)
 
